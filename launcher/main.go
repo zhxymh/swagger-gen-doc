1a8b3c9e705dcebf59ffb2d13f4107dae7b7a2a1
zhxymh <sean.zhang@aelf.io>
2020-12-05 16:02:50 +0800

    add flag

--- launcher/main.go
+++ launcher/main.go
@@ -1,34 +1,70 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
 
 	gendoc "github.com/zhxymh/swagger-gen-doc"
 )
 
 func main() {
-	println("Hello gendoc!")
+	inPtr := flag.String("doc_in", "resources/swagger.json", "the swagger json full path.")
+	outPtr := flag.String("doc_out", "./swagger.md", "the generated file path.")
+	optPtr := flag.String("doc_opt", "templates/markdown.tmpl", "the custome template file path.")
 
-	file, err := os.Open("resources/swagger.json")
+	flag.Parse()
+
+	content, err := readJsonContent(*inPtr)
 	if err != nil {
-		println(err)
+		log.Fatal(err)
+		return
+	}
+
+	if err = generateDoc(&content, *outPtr, *optPtr); err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	filePath, _ := filepath.Abs(*outPtr)
+	log.Println("Generated successfully! file path: " + filePath)
+}
+
+func readJsonContent(file string) ([]byte, error) {
+	jsonFile, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer jsonFile.Close()
+
+	content, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return content, nil
+}
+
+func generateDoc(json *[]byte, outFile string, tmplFile string) error {
+	templateData := gendoc.GenerateTemplateData(*json)
+	tmpl, err := template.ParseFiles(tmplFile)
+
+	if err != nil {
+		return err
+	}
+
+	file, err := os.OpenFile(outFile, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return err
 	}
 	defer file.Close()
 
-	content, err := ioutil.ReadAll(file)
-
-	templateData := gendoc.GenerateTemplateData(content)
-	println(templateData.Info.Version)
-
-	t1, err := template.ParseFiles("templates/test.tmpl")
-
-	if err != nil {
-		println(err.Error())
+	if err = tmpl.Execute(file, templateData); err != nil {
+		return err
 	}
 
-	_ = t1.Execute(os.Stdout, templateData)
-
-	println()
+	return nil
 }
---
+++ swagger.md
@@ -0,0 +1,7 @@
+# swagger gen doc 
+
+## AELF API 1.0
+  I am api doc.
+
+current version: 1.0
+
